fe6b0be8299d004c8b5c27b134ce307a101dac2a
althaf <althaf.sharafudheen@outlook.com>
2022-07-03 16:30:26 +0400

    XMLHttpRequest succesfully showing error case

--- request.js
+++ request.js
@@ -1,9 +1,10 @@
 const request =new XMLHttpRequest();
-request.open("GET", "https://jsonplaceholder.typicode.com/users");
+request.open("GET", "https://jsonplaceholder.typicode.com/users111");
 request.send();
 request.onload = ()=>{
     if(request.status === 200){//now the status is 200 then only the onload function will work
         console.log(JSON.parse(request.response));
-
+    }else{
+        console.log(`error ${request.status}`)
     }
 }
