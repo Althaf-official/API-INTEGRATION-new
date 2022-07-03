c1acb438ac31ce6e11a5fcb8fffa5b97df3822dc
althaf <althaf.sharafudheen@outlook.com>
2022-07-03 16:26:51 +0400

    XMLHttpRequest showing API object as a JSON Format

--- request.js
+++ request.js
@@ -3,7 +3,7 @@ request.open("GET", "https://jsonplaceholder.typicode.com/users");
 request.send();
 request.onload = ()=>{
     if(request.status === 200){//now the status is 200 then only the onload function will work
-        console.log(request.response);
+        console.log(JSON.parse(request.response));
 
     }
 }
