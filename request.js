e83f179f4ec310f5689fed6e17790f15c737211c
althaf <althaf.sharafudheen@outlook.com>
2022-07-03 16:38:01 +0400

    fetch API -showing fetch in the console (window.fetch)

--- request.js
+++ request.js
@@ -1,10 +1,16 @@
 const request =new XMLHttpRequest();
-request.open("GET", "https://jsonplaceholder.typicode.com/users111");
+request.open("GET", "https://jsonplaceholder.typicode.com/users");
 request.send();
 request.onload = ()=>{
     if(request.status === 200){//now the status is 200 then only the onload function will work
-        console.log(JSON.parse(request.response));
+        // console.log(JSON.parse(request.response));
     }else{
-        console.log(`error ${request.status}`)
+        // console.log(request.status);
+        // console.log(request);
+        // console.log(`error ${request.status}`)
     }
 }
+
+//fetch api
+
+console.log(window)
